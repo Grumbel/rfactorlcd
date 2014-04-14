ffbc95fd391b43dfee2a9af6ee29bf9cdfc7d9b4
Ingo Ruhnke <grumbel@gmail.com>
2014-04-14 18:41:02 +0200

    Implemented message parsing with construct, but it's horribly slow compared to hand parsing
    no parsing: 900kb/s (network bandwidth limit)
    hand parsing: 90kb/s
    construct parsing: 16kb/s

--- rfactorlcd/state.py
+++ rfactorlcd/state.py
@@ -15,6 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+from construct import Struct, UBInt8, SLInt16, SLInt32, LFloat32, Array, PascalString
+
 import rfactorlcd
 
 
@@ -30,161 +32,130 @@ class VehicleState(object):
         pass
 
 
+Vect = lambda name: Struct(name,  LFloat32("x"), LFloat32("y"), LFloat32("z"))
+
+InfoMessage = Struct("info",
+                     PascalString("track_name"),
+                     PascalString("player_name"),
+                     PascalString("plr_file_name"),
+                     LFloat32("end_e_t"),
+                     SLInt32("max_laps"),
+                     LFloat32("lap_dist"))
+
+ScoreMessage = Struct("score",
+                      UBInt8("game_phase"),
+                      UBInt8("yellow_flag_state"),
+                      Array(3, UBInt8("sector_flag")),
+                      UBInt8("start_light"),
+                      UBInt8("num_red_lights"),
+                      SLInt32("session"),
+                      LFloat32("current_e_t"),
+                      LFloat32("ambient_temp"),
+                      LFloat32("track_temp"))
+
+VehicleMessage = Struct("vehicles",
+                        SLInt32("num_vehicles"),
+                        Array(lambda ctx: ctx.num_vehicles,
+                              Struct("vehicle",
+                                     UBInt8("is_player"),
+                                     PascalString("driver_name"),
+                                     PascalString("vehicle_name"),
+                                     PascalString("vehicle_class"),
+                                     SLInt16("total_laps"),
+
+                                     UBInt8("in_pits"),
+                                     UBInt8("place"),
+                                     LFloat32("time_behind_next"),
+                                     SLInt32("laps_behind_next"),
+                                     LFloat32("time_behind_leader"),
+                                     SLInt32("laps_behind_leader"),
+
+                                     LFloat32("best_sector1"),
+                                     LFloat32("best_sector2"),
+                                     LFloat32("best_lap_time"),
+                                     LFloat32("last_sector1"),
+                                     LFloat32("last_sector2"),
+                                     LFloat32("last_lap_time"),
+                                     LFloat32("cur_sector1"),
+                                     LFloat32("cur_sector2"),
+
+                                     SLInt16("num_pitstops"),
+                                     SLInt16("num_penalties"),
+
+                                     LFloat32("lap_start_et"))))
+
+TelemetryMessage = Struct("telemetry",
+                          SLInt32("lap_number"),
+                          LFloat32("lap_start_et"),
+
+                          Vect("pos"),
+                          Vect("local_vel"),
+                          Vect("local_accel"),
+
+                          Vect("ori_x"),
+                          Vect("ori_y"),
+                          Vect("ori_z"),
+                          Vect("local_rot"),
+                          Vect("local_rot_accel"),
+
+                          SLInt32("gear"),
+                          LFloat32("rpm"),
+                          LFloat32("max_rpm"),
+                          LFloat32("clutch_rpm"),
+
+                          LFloat32("fuel"),
+                          LFloat32("water_temp"),
+                          LFloat32("oil_temp"),
+
+                          LFloat32("throttle"),
+                          LFloat32("brake"),
+                          LFloat32("steering"),
+                          LFloat32("clutch"),
+
+                          LFloat32("steering_arm_force"),
+
+                          UBInt8("scheduled_stops"),
+                          UBInt8("overheating"),
+                          UBInt8("detached"),
+
+                          Array(8, UBInt8("dent_severity")),
+
+                          Array(4,
+                                Struct("wheel",
+                                       LFloat32("rotation"),
+                                       LFloat32("suspension_deflection"),
+                                       LFloat32("ride_height"),
+                                       LFloat32("tire_load"),
+                                       LFloat32("lateral_force"),
+                                       LFloat32("grip_fract"),
+                                       LFloat32("brake_temp"),
+                                       LFloat32("pressure"),
+                                       Array(3, LFloat32("temperatur")),
+                                       LFloat32("wear"),
+                                       UBInt8("surface_type"),
+                                       UBInt8("flat"),
+                                       UBInt8("detached"))))
+
+
 class rFactorState(object):
 
-    def __init__(self, data="15/17,1,0.107,1.438,1.658,21.598,29.738,31.855,1:23.190,0.0,0,30.0,63.4,26.9,0.0,0.0"):
-        self.data = data
-
-        cols = data.split(",")
-        self.position = cols[0]
-        self.running = int(cols[1])
-        self.unknowns = cols[2:5]
-        self.sector = cols[5:8]
-        self.laptime = cols[8]
-        self.speed = float(cols[9])
-        self.gear = int(cols[10])
-        self.fuel = float(cols[11])
-        self.oil_temp = float(cols[12])
-        self.water_temp = float(cols[13])
-        self.rpm = float(cols[14])
-        self.max_rpm = float(cols[15])
-
-        self.wheels = [WheelState(), WheelState(), WheelState(), WheelState()]
-        self.vehicles = []
-
-    def to_vracingDisplayPRO(self):
-        result = "%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % (
-            self.position,
-            self.running,
-            self.unknowns[0],
-            self.unknowns[1],
-            self.unknowns[2],
-            self.sector[0],
-            self.sector[1],
-            self.sector[2],
-            self.laptime,
-            self.speed,
-            self.gear,
-            self.fuel,
-            self.oil_temp,
-            self.water_temp,
-            self.rpm,
-            self.max_rpm)
-
-        # print "IN: ", self.data
-        # print "OUT: ", result
-
-        return result
+    def __init__(self):
+        self.telemetry = None
+        self.vehicles = None
+        self.score = None
+        self.info = None
 
     def on_telemetry(self, msg):
-        self.lap_number = msg.read_int()
-        self.lap_start_et = msg.read_float()
-
-        self.pos = msg.read_vect()
-        self.local_vel = msg.read_vect()
-        self.speed = -self.local_vel[2]
-        self.local_accel = msg.read_vect()
-
-        self.ori_x = msg.read_vect()
-        self.ori_y = msg.read_vect()
-        self.ori_z = msg.read_vect()
-        self.local_rot = msg.read_vect()
-        self.local_rot_accel = msg.read_vect()
-
-        self.gear = msg.read_int()
-        self.rpm = msg.read_float()
-        self.max_rpm = msg.read_float()
-        self.clutch_rpm = msg.read_float()
-
-        self.fuel = msg.read_float()
-        self.water_temp = msg.read_float()
-        self.oil_temp = msg.read_float()
-
-        self.throttle = msg.read_float()
-        self.brake = msg.read_float()
-        self.steering = msg.read_float()
-        self.clutch = msg.read_float()
-
-        self.steering_arm_force = msg.read_float()
-
-        self.scheduled_stops = msg.read_char()
-        self.overheating = msg.read_char()
-        self.detached = msg.read_char()
-
-        self.dent_severity = msg.read_multi_char(8)
-
-        for i in range(0, 4):
-            self.wheels[i].rotation = msg.read_float()
-            self.wheels[i].suspension_deflection = msg.read_float()
-            self.wheels[i].ride_height = msg.read_float()
-            self.wheels[i].tire_load = msg.read_float()
-            self.wheels[i].lateral_force = msg.read_float()
-            self.wheels[i].grip_fract = msg.read_float()
-            self.wheels[i].brake_temp = msg.read_float()
-            self.wheels[i].pressure = msg.read_float()
-            self.wheels[i].temperature = [msg.read_float(),
-                                          msg.read_float(),
-                                          msg.read_float()]
-            self.wheels[i].wear = msg.read_float()
-            self.wheels[i].surface_type = msg.read_char()
-            self.wheels[i].flat = msg.read_char()
-            self.wheels[i].detached = msg.read_char()
+        self.telemetry = TelemetryMessage.parse(msg)
 
     def on_vehicle(self, msg):
-        self.num_vehicles = msg.read_int()
-
-        if self.num_vehicles < len(self.vehicles):
-            self.vehicles = self.vehicles[:self.num_vehicles]
-        elif self.num_vehicles > len(self.vehicles):
-            for i in range(self.num_vehicles - len(self.vehicles)):
-                self.vehicles.append(VehicleState())
-
-        for i in range(0, self.num_vehicles):
-            self.vehicles[i].is_player = msg.read_char()
-            self.vehicles[i].driver_name = msg.read_string()
-            self.vehicles[i].vehicle_name = msg.read_string()
-            self.vehicles[i].vehicle_class = msg.read_string()
-            self.vehicles[i].total_laps = msg.read_short()
-
-            self.vehicles[i].in_pits = msg.read_char()
-            self.vehicles[i].place = msg.read_char()
-            self.vehicles[i].time_behind_next = msg.read_float()
-            self.vehicles[i].laps_behind_next = msg.read_int()
-            self.vehicles[i].time_behind_leader = msg.read_float()
-            self.vehicles[i].laps_behind_leader = msg.read_int()
-
-            self.vehicles[i].best_sector1 = msg.read_float()
-            self.vehicles[i].best_sector2 = msg.read_float()
-            self.vehicles[i].best_lap_time = msg.read_float()
-            self.vehicles[i].last_sector1 = msg.read_float()
-            self.vehicles[i].last_sector2 = msg.read_float()
-            self.vehicles[i].last_lap_time = msg.read_float()
-            self.vehicles[i].cur_sector1 = msg.read_float()
-            self.vehicles[i].cur_sector2 = msg.read_float()
-
-            self.vehicles[i].num_pitstops = msg.read_short()
-            self.vehicles[i].num_penalties = msg.read_short()
-
-            self.vehicles[i].lap_start_et = msg.read_float()
+        self.vehicles = VehicleMessage.parse(msg)
 
     def on_score(self, msg):
-        self.game_phase = msg.read_char()
-        self.yellow_flag_state = msg.read_char()
-        self.sector_flag = msg.read_multi_char(3)
-        self.start_light = msg.read_char()
-        self.num_red_lights = msg.read_char()
-        self.session = msg.read_int()
-        self.current_e_t = msg.read_float()
-        self.ambient_temp = msg.read_float()
-        self.track_temp = msg.read_float()
+        self.score = ScoreMessage.parse(msg)
 
     def on_info(self, msg):
-        self.track_name = msg.read_string()
-        self.player_name = msg.read_string()
-        self.plr_file_name = msg.read_string()
-        self.end_e_t = msg.read_float()
-        self.max_laps = msg.read_int()
-        self.lap_dist = msg.read_float()
+        self.info = InfoMessage.parse(msg)
 
     def on_start_realtime(self, msg):
         pass
@@ -199,24 +170,22 @@ class rFactorState(object):
         pass
 
     def dispatch_message(self, tag, payload):
-        msg = rfactorlcd.BinaryDecoder(payload)
-
         if tag == "STSS":
-            self.on_start_session(msg)
+            self.on_start_session(payload)
         elif tag == "EDSS":
-            self.on_end_session(msg)
+            self.on_end_session(payload)
         elif tag == "STRT":
-            self.on_start_realtime(msg)
+            self.on_start_realtime(payload)
         elif tag == "EDRT":
-            self.on_end_realtime(msg)
+            self.on_end_realtime(payload)
         elif tag == "VHCL":
-            self.on_vehicle(msg)
+            self.on_vehicle(payload)
         elif tag == "TLMT":
-            self.on_telemetry(msg)
+            self.on_telemetry(payload)
         elif tag == "SCOR":
-            self.on_score(msg)
+            self.on_score(payload)
         elif tag == "INFO":
-            self.on_info(msg)
+            self.on_info(payload)
         else:
             print "error: unhandled tag: %s" % tag
 
