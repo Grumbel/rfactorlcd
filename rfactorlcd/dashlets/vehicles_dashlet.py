08059c78c2330f150c54f414b952cb0ddadae60b
Ingo Ruhnke <grumbel@gmail.com>
2014-04-16 22:29:38 +0200

    Added some better dynamic resize for VehiclesDashlet

--- rfactorlcd/dashlets/vehicles_dashlet.py
+++ rfactorlcd/dashlets/vehicles_dashlet.py
@@ -19,12 +19,14 @@ import rfactorlcd
 
 
 def parse_time(v):
+    if v == 0 or v == -1.0:
+        return "--:--:---"
     minutes = int(v / 60)
     v -= minutes * 60
     seconds = int(v)
     v -= seconds
     mili = int(v * 1000)
-    return (minutes, seconds, mili)
+    return "%2d:%02d:%03d" % (minutes, seconds, mili)
 
 
 class VehiclesDashlet(rfactorlcd.Dashlet):
@@ -32,6 +34,21 @@ class VehiclesDashlet(rfactorlcd.Dashlet):
     def __init__(self, *args):
         super(VehiclesDashlet, self).__init__(*args)
         self.vehicles = []
+        self.font_size = self.h / 32.0
+        self.column_width = [5, 22, 10, 10, 10, 10, 10]
+        self.column_offset = self.column_width
+
+    def reshape(self, x, y, w, h):
+        self.font_size = h / 32.0
+
+        total_w = 0
+        self.column_offset = []
+        for w in self.column_width:
+            self.column_offset.append(total_w)
+            total_w += w
+
+        for i in range(0, len(self.column_offset)):
+            self.column_offset[i] = self.column_offset[i] / float(total_w) * self.w
 
     def update_state(self, state):
         self.vehicles = state.vehicles
@@ -39,18 +56,35 @@ class VehiclesDashlet(rfactorlcd.Dashlet):
 
     def draw(self, cr):
         if len(self.vehicles) > 0:
-            font_size = 25  # self.h / len(self.vehicles)
-            y = font_size
-            cr.set_font_size(font_size)
+            cr.set_font_size(self.font_size)
+
+            cr.set_source_rgb(*self.lcd_style.highlight_color)
+            self.draw_row(cr, 0, 1, "Name")
+            self.draw_row(cr, 0, 2, " Best")
+            self.draw_row(cr, 0, 3, " Last")
+            self.draw_row(cr, 0, 4, " Sector1")
+            self.draw_row(cr, 0, 5, " Sector2")
+            self.draw_row(cr, 0, 6, " Sector3")
+
             for veh in sorted(self.vehicles, lambda a, b: cmp(a.place, b.place)):
                 if veh.is_player:
                     cr.set_source_rgb(*self.lcd_style.highlight_color)
                 else:
                     cr.set_source_rgb(*self.lcd_style.foreground_color)
 
-                cr.move_to(0, veh.place * font_size)
-                cr.show_text("%2d %-20s %2d:%02d:%03d" % ((veh.place, veh.driver_name) + parse_time(veh.last_lap_time)))
-                y += font_size
+                row = veh.place
+                self.draw_row(cr, row, 0, "%2d" % veh.place)
+                self.draw_row(cr, row, 1, veh.driver_name)
+                self.draw_row(cr, row, 2, parse_time(veh.best_lap_time))
+                self.draw_row(cr, row, 3, parse_time(veh.last_lap_time))
+                self.draw_row(cr, row, 4, parse_time(veh.last_sector1))
+                self.draw_row(cr, row, 5, parse_time(veh.last_sector2 - veh.last_sector1))
+                self.draw_row(cr, row, 6, parse_time(veh.last_lap_time - veh.last_sector2))
+
+    def draw_row(self, cr, row, col, text):
+            cr.move_to(self.column_offset[col], (row + 1.2) * self.font_size * 1.2)
+
+            cr.show_text(text)
 
 
 # EOF #
